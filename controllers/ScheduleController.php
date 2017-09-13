e46f5fda49a7ece1b45b149852b07576927cc0be
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2017-09-13 17:59:23 +0700

    Added schedule feature
    add more shift
    remove shift from schedule

--- controllers/ScheduleController.php
+++ controllers/ScheduleController.php
@@ -2,6 +2,7 @@
 use Model\Schedules;
 use Model\Users;
 use Model\Shifts;
+use Model\Schedule_details;
 class ScheduleController{
 	public function all(){
 		header('Content-type: application/json');
@@ -70,6 +71,8 @@ class ScheduleController{
 										":" . 
 										$timeEndParts["minute"];
 				$shift["timeAt"] = $timeAt;
+				$shift["ShiftId"] = $schedule["ShiftId"];
+				$shift["Date"] = $schedule["Date"];
 				array_push($shifts_details[$dayOfWeek], $shift);
 				// sort shifts
 				usort($shifts_details[$dayOfWeek], function ( $a, $b ) {
@@ -78,6 +81,7 @@ class ScheduleController{
 			}
 
 			array_push($data, [
+				"Uid" => $employee->Id,
 				"DisplayName" => $employee->DisplayName, 
 				"Shifts" => $shifts_details,
 				]);
@@ -86,13 +90,76 @@ class ScheduleController{
 		$otherSchedule = Schedules::all()->toArray();
 		// is admin
 		$isAdmin = $_SESSION["Role"] == ADMIN_ROLE;
+		// all shift
+		$allShifts = Shifts::all();
 		// render
 		View("ScheduleDetails", [
 			"schedules" => $data, 
 			"scheduleInfo" => $scheduleInfo,
 			"otherSchedule" => $otherSchedule,
-			"isAdmin" => $isAdmin
+			"isAdmin" => $isAdmin,
+			"employees" => $employees,
+			"allShifts" => $allShifts,
 			]);
+	}
+
+	public function deleteShift(){
+		$uid     = $_POST['uid'];
+		$shiftId = $_POST['shiftId'];
+		$date    = $_POST['date'];
+		if($_SESSION['Role'] == ADMIN_ROLE){
+			Schedule_details::where('Date', '=', $date)
+							->where('ShiftId', '=', $shiftId)
+							->where('UserId', '=', $uid)
+							->delete();
+			echo "success";
+		}
+		else{
+			echo "nope";
+		}
+	}
+
+	public function addShift(){
+		$scheduleId = $_POST['scheduleId'];
+		$dayOfWeek  = $_POST['dayOfWeek'];
+		$shift      = $_POST['shift'];
+		$employeeId = $_POST['employeeId'];
+
+		$schedule = Schedules::find($scheduleId);
+		if($_SESSION['Role'] == ADMIN_ROLE){
+			$details = new Schedule_details;
+			$details->Schedule_id = $scheduleId;
+			$details->DayOfWeek = $dayOfWeek;
+			$details->UserId = $employeeId;
+			$details->ShiftId = $shift;
+			$date = date('Y-m-d', strtotime($schedule->Date_start.' + ' . $dayOfWeek . ' days'));
+			$details->Date = $date;
+			$details->save();
+			$shiftDetails = Shifts::find($shift)->toArray();
+			$timeStartParts = date_parse($shiftDetails["Time_start"]);
+			$timeEndParts = date_parse($shiftDetails["Time_end"]);
+			$timeAt = "";
+			if($timeStartParts["hour"] < 12){
+				$timeAt = "morning";
+			}
+			else if($timeStartParts["hour"] < 17){
+				$timeAt = "afternoon";
+			}
+			else{
+				$timeAt = "night";
+			}
+			
+			$html = "<div class='shift-panel ".$timeAt."'>";
+			$html .= "<div class='name text-center'>";
+			$html .= $shiftDetails["Name"];
+			$html .= "<div class='time text-center'>";
+			$html .= $shiftDetails["Time_start"] . " - " . $shiftDetails["Time_end"];
+			$html .= "</div>";
+			$html .= "<div class='text-center' style='font-size: 1.5em'>";
+			$html .= "<i class='fa fa-trash' data-toggle='modal' data-target='#deleteModal' style='cursor: pointer' data-uid='".$employeeId."' data-date='". $date. "' data-shiftId='".$shift."'></i>";
+			$html .= "</div>";
+			echo $html;
+		}		
 	}
 }
 ?>
